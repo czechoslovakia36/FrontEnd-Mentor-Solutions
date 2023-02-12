d846c4f9c16b0c31f0224fe020e55a0472a14489
Vivek Kumar <clocktower77@protonmail.com>
2023-02-13 00:11:12 +0530

    tweaking with code #2

--- interactive-rating-component/interactive-rating-app-nonreact/index.js
+++ interactive-rating-component/interactive-rating-app-nonreact/index.js
@@ -14,15 +14,21 @@ console.log(e)
 
 
 
-console.log(target)
+// target[i].addEventListener("click",(e)=> {
 
-
-console.log(target.length)
-
+//    target[i].classList.toggle("select")
+// })
 
 for( var i=0;i<target.length;i++){
-    console.log(target.classList)
-    target[i].addEventListener("click",(e)=> {
-       target[i].classList.add("select")
+    console.log(target[i])
+    target[i].addEventListener("click",(e) => {
+        console.log(e.target)
+
+        if(e.target.classList.contains("no")){
+            e.target.parentElement.classList.toggle("select")
+        }
+     
+       
     })
+
 }

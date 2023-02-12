725a81e35c0c2d796dfb4d749bf0a7baa295c43a
Vivek Kumar <clocktower77@protonmail.com>
2023-02-12 23:39:26 +0530

    tweaking with script

--- interactive-rating-component/interactive-rating-app-nonreact/index.js
+++ interactive-rating-component/interactive-rating-app-nonreact/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("click", (e)=> {
-    console.log(e.target)
-})
+// document.addEventListener("click", (e)=> {
+//     console.log(e.target)
+// })
+
+const target= document.getElementsByTagName("i")
+
+
+
+// To get classList of The element
+let e= document.getElementById("btn")
+console.log(e)
+// // alert(e.classList)
+// console.log(e.classList)
+
+
+
+console.log(target)
+
+
+console.log(target.length)
+
+
+for( var i=0;i<target.length;i++){
+    console.log(target.classList)
+    target[i].addEventListener("click",(e)=> {
+       target[i].classList.add("select")
+    })
+}

4264dff2872d1f29e23aaccbad7b3a849e880f1a
Vivek Kumar <clocktower77@protonmail.com>
2023-02-13 15:10:22 +0530

    final script logic created

--- interactive-rating-component/interactive-rating-app-nonreact/index.js
+++ interactive-rating-component/interactive-rating-app-nonreact/index.js
@@ -7,10 +7,61 @@ const target= document.getElementsByTagName("i")
 
 
 // To get classList of The element
-let e= document.getElementById("btn")
-console.log(e)
+let iconTag= document.getElementById("btn")
+let p = document.getElementsByClassName("no")
+// console.log(e)
 // // alert(e.classList)
 // console.log(e.classList)
+const ratings2= [...p]
+console.log(ratings2[0])
+const ratings = [...target]
+
+
+// ratings2.map(rating => {
+//     console.log(rating)
+//     rating.onclick = () => {
+       
+//        rating.classList.add("active")
+//        index= ratings2.indexOf(rating) 
+//        console.log(index)
+//        console.log(rating)
+//         for (var i=0 ; i<ratings2.length;i++){
+//             if(i==index){
+                
+//             }
+//             else {
+//                 rating.classList.remove("active")
+//             }
+//         }
+       
+       
+//     }
+// }
+//    )
+
+ratings.map(rating => {
+   
+    rating.onclick = () => {
+       
+       rating.classList.add("active")
+       index= ratings.indexOf(rating) 
+       console.log(index)
+       console.log(rating)
+        for (var i=0 ; i<ratings.length;i++){
+            if(i==index){
+                
+            }
+            else {
+                ratings[i].classList.remove("active")
+            }
+        }
+       
+       
+    }
+}
+   )
+
+
 
 
 
@@ -19,16 +70,19 @@ console.log(e)
 //    target[i].classList.toggle("select")
 // })
 
-for( var i=0;i<target.length;i++){
-    console.log(target[i])
-    target[i].addEventListener("click",(e) => {
-        console.log(e.target)
+// for( var i=0;i<target.length;i++){
+//     console.log(target[i])
+//     target[i].addEventListener("click",(e) => {
+//         console.log(e.target)
 
-        if(e.target.classList.contains("no")){
-            e.target.parentElement.classList.toggle("select")
-        }
+//         e.target.classList.remove("select")
+    
+
+//         if(e.target.classList.contains("no") ){
+//             e.target.parentElement.classList.add("select")
+//         }
      
        
-    })
+//     })
 
-}
+// }

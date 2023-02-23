8620ee7071f96c93b0cdaa6acf9ec1c499d2d536
Vivek Kumar <clocktower77@protonmail.com>
2023-02-23 18:02:05 +0530

    initial commit

---
+++ multi-step-form/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import YourInfo from './components/YourInfo';
+
+function App() {
+  return (
+    <>
+    <YourInfo/>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ multi-step-form/src/components/YourInfo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class YourInfo extends Component {
+    render() {
+        return (
+            <div>
+                Hello Step 1
+            </div>
+        );
+    }
+}
+
+export default YourInfo;

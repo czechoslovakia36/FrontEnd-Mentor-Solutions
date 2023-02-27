43af738cc725b29af6cdf53d0107a2c88c519eea
Vivek Kumar <clocktower77@protonmail.com>
2023-02-27 15:23:04 +0530

    outer layout created

--- multi-step-form/src/components/YourInfo.jsx
+++ multi-step-form/src/components/YourInfo.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 const YourInfo = () => {
     return (
-        <div>
-           hello
+        <>
+        <div className='main-container flex justify-center align-center'>
+            <div className='container flex test-red pad-2'>
+                <div className='w-20 flex'>
+                        <div className=' circle'></div>
+                   
+                </div>
+                <div className='test-green w-80 ml-100'>World</div>
+            </div>
         </div>
+           
+        </>
     );
 }
 
